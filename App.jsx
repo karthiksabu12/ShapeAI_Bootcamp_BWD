0e4bfca8ca472700b62c89010d836466f3ea1761
karthiksabu12 <85401742+karthiksabu12@users.noreply.github.com>
2021-06-05 19:37:57 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Notes from "./Notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Notes />
+    </div>
+  );
+}
+export default App;
---
+++ Notes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Notes(){
+  return <div className="note">
+      <h1>
+        JavaScript and React.js
+      </h1>  
+      <p>JavaScript, often abbreviated as JS, 
+        is a programming language that conforms
+         to the ECMAScript specification and React 
+         is an open-source front-end JavaScript 
+         library for building user interfaces or 
+         UI components </p>
+    </div>;
+}
+export default Notes;
